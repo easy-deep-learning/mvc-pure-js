6de68733589b6ad9e986c7a9fe872a6eee04fb55
Alex Baumgertner <alex.baumgertner@gmail.com>
2016-03-06 14:30:38 +0300

    Add docs to js code

--- js/common.js
+++ js/common.js
@@ -1,18 +1,28 @@
+console.log("Loaded in: ", this);
+
 /**
  *
  * @type {{}}
  */
-var Router = {};
+var Router = {
+  // https://developer.mozilla.org/en-US/docs/Web/API/History_API
+};
 
 
 /**
  *
  * @type {{}}
  */
-var Model = {};
+var Model = {
+  // https://addyosmani.com/resources/essentialjsdesignpatterns/book/#observerpatternjquery
+  // see https://carldanley.com/js-observer-pattern/
+};
 
 /**
  *
  * @type {{}}
  */
-var view = {};
+var View = {
+  // Write by hand (no vendor template engine
+  // see http://ejohn.org/blog/javascript-micro-templating/
+};
