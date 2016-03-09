b513bdf945985f2a08e27a6602145624a5ea20c3
Dmitry <dimitrylebedev@yandex.ru>
2016-03-09 12:51:15 +0300

    Remove template polyfill

--- js/common.js
+++ js/common.js
@@ -4,24 +4,6 @@
 	var App = global.App;
 	var doc = global.document;
 
-	/**
-	 * <template> Polyfill
-	 *
-	 * @param {Element} templateNode
-	 * @return {Template}
-	 */
-	function getTemplateFromNode(templateNode) {
-		var template;
-
-		if ('content' in templateNode) {
-			template = templateNode.content.children[0].cloneNode(true);
-		} else {
-			template = templateNode.children[0].cloneNode(true);
-		}
-
-		return template;
-	}
-
 	var dataStub = {
 		events: [
 			{
@@ -42,12 +24,10 @@
 	};
 
 	var templateEventsItemNode = doc.getElementById('template_events__item');
-	//var templateEventsItem = getTemplateFromNode(templateEventsItemNode)
 
 	var eventsUI = new App.View(templateEventsItemNode.innerHTML);
 	var eventsHTML = eventsUI.render(dataStub);
 
-	//console.log("templateEventsItem: ", templateEventsItem.innerHTML);
 	console.log('templateEventsItemNode', templateEventsItemNode.innerHTML);
 
 	doc.querySelector('.events').innerHTML = eventsHTML;
