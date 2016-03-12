c2e5636cde21a80e414a4b2f332f983c3b20e7d2
Alex Baumgertner <alex.baumgertner@gmail.com>
2016-03-12 21:54:50 +0300

    Tests: Add invalid template
    Add invalid template throw test
    http://chaijs.com/api/bdd/#method_throw

--- tests/view.spec.js
+++ tests/view.spec.js
@@ -36,7 +36,6 @@ describe('View', function() {
   });
 
   it('should set tempalte (with JS code)', function () {
-
     testView = new View (
       '<% dataItems.forEach(function(item) { %>' +
         '<%= item.value %>' +
@@ -48,7 +47,17 @@ describe('View', function() {
     var reference = 'testItem0testItem1';
 
     expect(result).to.equal(reference);
+  });
 
+  it('should throw error with invalid template', function() {
+    testView = new View('<%= name'); // template is not closed
+
+    var testData = {name: 'test'};
+    var renderRunner = function() {
+      testView.render(testData);
+    };
+
+    expect(renderRunner).to.throw(Error);
   });
 
 });
